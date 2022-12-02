a1d4d4a1f3b87cd7ed88f0ec27fe114beb421735
NoeliaxRodriguez <100428957@alumnos.uc3m.es>
2022-12-02 17:03:53 +0100

    finalizado y probado en postman funcionando correctamente

--- src/main/java/com/grupo06/eve8206/Eve8206Application.java
+++ src/main/java/com/grupo06/eve8206/Eve8206Application.java
@@ -1,11 +1,10 @@
 package com.grupo06.eve8206;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Eve8206Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Eve8206Application.class, args);
     }
--- src/main/java/com/grupo06/eve8206/controllers/EventController.java
+++ src/main/java/com/grupo06/eve8206/controllers/EventController.java
@@ -5,10 +5,13 @@ import com.grupo06.eve8206.repos.EventDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
+@Controller
 public class EventController {
     static final String basePath = "/restful";
 
